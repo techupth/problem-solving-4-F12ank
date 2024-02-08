7445cb5d1a0ffe9f0c4ec564f75f08750c583037
nattawat <frank.nattawat@hotmail.com>
2024-02-08 18:12:24 +0700

    complete ex

--- ex-1.js
+++ ex-1.js
@@ -1,1 +1,26 @@
-function pivotIndex(nums) {}
+function pivotIndex(nums, input) {
+  console.log(nums, input);
+  let leftSum = 0;
+  let leftSumText = "";
+  let rightSum = 0;
+  let reverseNumber = nums.reverse();
+  let rightSumText = "";
+
+  for (let i = 0; i < input - 1; i++) {
+    leftSum = nums[i] + leftSum;
+    leftSumText = leftSumText + `nums[${i}] + `;
+    // console.log(result)
+  }
+  for (let i = 0; i < nums.length; i++) {
+    if (leftSum > rightSum) {
+      rightSum = rightSum + reverseNumber[i];
+      rightSumText = `nums[${nums.length - i}] + ` + rightSumText;
+    }
+  }
+
+  return `left sum = ${leftSumText} = ${leftSum},
+          right sum = ${rightSumText} = ${rightSum}`;
+}
+
+let result1 = pivotIndex([1, 7, 3, 6, 5, 6], 3);
+console.log(result1);
--- ex-2.js
+++ ex-2.js
@@ -1,1 +1,11 @@
-function defangIPaddr(nums) {}
+function defangIPaddr(nums) {
+  let newAddress = nums.split(".").join("[.]");
+  console.log(newAddress);
+  return newAddress;
+}
+
+let address1 = defangIPaddr("1.1.1.1");
+let address2 = defangIPaddr("255.100.50.0");
+
+console.log(address1);
+console.log(address2);
